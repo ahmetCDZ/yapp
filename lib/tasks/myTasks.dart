c3e091f96f55ad99398a4303574aef7f4d400ea8
Ahmet Zincir <ahmetzincir27@gmail.com>
2022-12-11 23:27:45 +0300

    sharedPreferences

--- lib/tasks/myTasks.dart
+++ lib/tasks/myTasks.dart
@@ -1,11 +1,37 @@
+import 'package:flutter/foundation.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:yapp/tasks/tasks.dart';
 
-class MyTasks {
-  final List<Tasks> tasks = [
-    Tasks(title: 'Elkektri öde'),
-    Tasks(title: 'Su öde'),
-  ];
+class MyTasks with ChangeNotifier {
+  late SharedPreferences sharedPreferencesObject;
+  List<Tasks> tasks = [Tasks(title: 'Ahmet')];
+
   void addTasks(String newTask) {
     tasks.add(Tasks(title: newTask));
+    savedTasks(tasks);
+    notifyListeners();
+  }
+
+  void markCheck(int index) {
+    tasks[index].markCheck();
+    notifyListeners();
+  }
+
+  void deletedTask(int index) {
+    tasks.removeAt(index);
+    notifyListeners();
+  }
+
+  Future<void> createPref() async {
+    sharedPreferencesObject = await SharedPreferences.getInstance();
+  }
+
+  savedTasks(List<Tasks> value) {
+    sharedPreferencesObject.setStringList('tasks', value);
+  }
+
+  void loadTasks() async {
+    await createPref();
+    tasks = sharedPreferencesObject.getStringList('tasks');
   }
 }
